4ad11e3d8c29dbff2e32ba813a0e6a58253bd6cc
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2024-01-02 22:23:25 +0330

    Add files via upload

---
+++ ConsoleApp156/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp156
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var string1 = "Happy";
+            var string2 = "Birthday";
+
+            Console.WriteLine($"string1=\"{string1}\"");
+            Console.WriteLine($"string2=\"{string2}\"");
+
+            Console.WriteLine($"\nResult of string.concat(string1,string2)=" + string.Concat(string1, string2));
+            Console.WriteLine($"string1 after concatination={string1}");
+            Console.ReadLine();
+            
+            
+        }
+    }
+}
